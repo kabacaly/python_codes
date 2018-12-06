0432a2d62f7d45e0055b51e20c2ef0baca540846
kabacaly <tkabacaly@gmail.com>
2018-12-06 23:55:59 +0300

    Update get_data_frame_null_percentage.py

--- get_data_frame_null_percentage.py
+++ get_data_frame_null_percentage.py
@@ -1,7 +1,7 @@
 def null_df(df, sort=True):
     nulls = df.isnull().sum()
     df_nulls = pd.DataFrame({'col':nulls.index.values, 'val':nulls.values})
-    df_nulls['perc'] = df_nulls['val'] / data.shape[0] * 100
+    df_nulls['perc'] = df_nulls['val'] / df.shape[0] * 100
     if sort:
         df_nulls.sort_values('perc', ascending=False, inplace=True)
     return df_nulls
