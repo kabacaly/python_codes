0d6a129a00ae53b7ad2c727ba50f73c5d7482ad6
kabacaly <tkabacaly@gmail.com>
2019-10-18 15:00:40 +0300

    add logging module

--- print_exception.py
+++ print_exception.py
@@ -1,11 +1,14 @@
 def PrintException():
     import sys
     import linecache
+    import logging
+    logging.basicConfig(format="%(asctime)s %(levelname)s: %(message)s", level=logging.ERROR, datefmt='%Y-%m-%d %I:%M:%S')
     exc_type, exc_obj, tb = sys.exc_info()
     f = tb.tb_frame
     lineno = tb.tb_lineno
     filename = f.f_code.co_filename
     linecache.checkcache(filename)
     line = linecache.getline(filename, lineno, f.f_globals)
-    print('EXCEPTION IN ({}, LINE {} "{}"): {}'.format(filename, lineno, line.strip(), exc_obj))
-    return 'EXCEPTION IN ({}, LINE {} "{}"): {}'.format(filename, lineno, line.strip(), exc_obj)  
+    msg = 'EXCEPTION IN ({}, LINE {} "{}"): {}'.format(filename, lineno, line.strip(), exc_obj)
+    logging.error(msg)
+    return msg
